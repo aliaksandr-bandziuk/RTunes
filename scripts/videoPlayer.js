fa5dff300006592ed75e0a20426f52d3a069efc5
Александр Бандюк <Bandziuk@Lux.local>
2020-07-28 17:04:41 +0300

    videoPlayer done

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,5 +1,80 @@
 'use strict';
 
 export const videoPlayerInit = () => {
-    console.log('Video Init');
+
+const videoPlayer = document.querySelector('.video-player'),
+    videoButtonPlay = document.querySelector('.video-button__play'),
+    videoButtonStop = document.querySelector('.video-button__stop'),
+    videoProgress = document.querySelector('.video-progress'),
+    videoTimePassed = document.querySelector('.video-time__passed'),
+    videoTimeTotal = document.querySelector('.video-time__total');
+
+    const toggleIcon = () => {
+        if (videoPlayer.paused) {
+            videoButtonPlay.classList.remove('fa-pause');
+            videoButtonPlay.classList.add('fa-play');
+        } else {
+            videoButtonPlay.classList.add('fa-pause');
+            videoButtonPlay.classList.remove('fa-play');
+        }
+    };
+
+    const togglePlay = () => {
+        if (videoPlayer.paused) {
+            videoPlayer.play();
+        } else {
+            videoPlayer.pause();
+        }
+        toggleIcon();
+    };
+
+    const stopPlay = () => {
+        videoPlayer.pause();
+        videoPlayer.currentTime = 0;
+    };
+
+    // чтобы на видео перед цифрами появлялся нолик
+    // для корректного подсчета минут
+    const addZero = n => n < 10 ? '0'+ n : n;
+
+    // const addZero = n => {
+    //     return n < 10 ? '0'+ n : n;
+    // };
+
+    videoPlayer.addEventListener('click', togglePlay);
+    videoButtonPlay.addEventListener('click', togglePlay);
+
+    videoPlayer.addEventListener('play', toggleIcon);
+    videoPlayer.addEventListener('pause', toggleIcon);
+
+    videoButtonStop.addEventListener('click', stopPlay);
+
+    videoPlayer.addEventListener('timeupdate', () => {
+        const currentTime = videoPlayer.currentTime;
+        const duration = videoPlayer.duration;
+
+        // инпут range движется
+        videoProgress.value = (currentTime / duration) * 100;
+
+        let minutePassed = Math.floor(currentTime / 60);
+        let secondsPassed = Math.floor(currentTime % 60);
+
+        let minuteTotal = Math.floor(duration / 60);
+        let secondsTotal = Math.floor(duration % 60);
+
+        videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`;
+        videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondsTotal)}`;
+
+        // альтернативный вариант
+        videoTimePassed.textContent = addZero(minutePassed) + ':' + addZero(secondsPassed);
+        videoTimeTotal.textContent = addZero(minuteTotal) + ':' + addZero(secondsTotal);
+    });
+
+    // при нажатии на бегунок перематывается видео
+    videoProgress.addEventListener('change', () => {
+        const duration = videoPlayer.duration;
+        const value = videoProgress.value;
+
+        videoPlayer.currentTime = (value * duration) / 100;
+    });
 };
